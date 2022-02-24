ceefd654120574ae4a1a941ef03c03b0815ddd9c
Anatolijs <a.morozovs@gmail.com>
2022-02-24 14:32:52 +0200

    redezigning password item

--- app/src/main/java/lv/maros/secured/password/keeper/views/PasswordTextView.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/views/PasswordTextView.kt
@@ -1,19 +1,8 @@
 package lv.maros.secured.password.keeper.views
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Point
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.ContextCompat
-import lv.maros.secured.password.keeper.R
-import timber.log.Timber
 
 class PasswordTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
@@ -24,49 +13,11 @@ class PasswordTextView @JvmOverloads constructor(
 
     private lateinit var encryptedPassword: String
 
-    var listener: OnPasswordVisibilityClockListener? = null
-
-    //TODO rework drawables to attributes !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    private val iconVisibleOff: Drawable =
-        ContextCompat.getDrawable(context, R.drawable.outline_visibility_off_black_18)!!
-    private val iconVisible: Drawable =
-        ContextCompat.getDrawable(context, R.drawable.outline_visibility_black_18)!!
-
-    private var iconVisibleOffBounds: Rect? = null
-
-    private val onPasswordTouchListener: OnTouchListener =
-        OnTouchListener { v, e ->
-            v.performClick()
-            processOnTouch(v, e)
-        }
-
-    private val gestureDetector: GestureDetector
-
-    private val gestureListener: GestureDetector.SimpleOnGestureListener =
-        object : GestureDetector.SimpleOnGestureListener() {
-            override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
-                iconVisibleOffBounds?.let {
-                    return if (it.contains(e.x.toInt(), e.y.toInt())) {
-                        changeVisibility()
-                        processVisibilityClick()
-                        invalidate() //TODO MOVE UP ?
-                        true
-                    } else {
-                        false
-                    }
-                }
-
-                return false
-            }
-        }
 
     init {
         isClickable = true
-        isSingleLine = true
         isPasswordVisible = false
 
-        gestureDetector = GestureDetector(context, gestureListener)
-        setOnTouchListener(onPasswordTouchListener)
     }
 
     override fun setText(text: CharSequence?, type: BufferType?) {
@@ -81,28 +32,7 @@ class PasswordTextView @JvmOverloads constructor(
         super.setText(newText, type)
     }
 
-    private fun processOnTouch(view: View, e: MotionEvent): Boolean {
-        val point = Point(e.rawX.toInt(), e.rawY.toInt())
-
-        val rect = Rect()
-        view.getGlobalVisibleRect(rect)
-
-        return if (
-            e.action == MotionEvent.ACTION_DOWN ||
-            e.action == MotionEvent.ACTION_UP ||
-            e.action == MotionEvent.ACTION_MOVE
-        ) {
-            if (rect.top < point.y && rect.bottom > point.y) {
-                gestureDetector.onTouchEvent(e)
-            } else {
-                false
-            }
-        } else {
-            false
-        }
-    }
-
-    private fun processVisibilityClick() {
+    /*private fun processVisibilityClick() {
         listener?.let {
             val data = if (isPasswordVisible) {
                 it.invoke(isPasswordVisible, encryptedPassword)
@@ -112,43 +42,12 @@ class PasswordTextView @JvmOverloads constructor(
             Timber.d("received data = $data")
             text = data
         }
-    }
+    }*/
 
     private fun changeVisibility() {
         isPasswordVisible = !isPasswordVisible
     }
 
-    @SuppressLint("DrawAllocation")
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        val icon = if (isPasswordVisible) {
-            iconVisible
-        } else {
-            iconVisibleOff
-        }
-        //TODO remove object creation
-        icon.bounds = Rect(
-            measuredWidth - icon.intrinsicWidth,
-            paddingTop,
-            measuredWidth,
-            measuredHeight
-        )
-
-        iconVisibleOffBounds = icon.bounds
-
-        icon.draw(canvas)
-    }
-
-    //TODO rework argument ?
-    fun setOnPasswordVisibilityClickListener(block: (isVisible: Boolean, data: String) -> String) {
-        isClickable = true
-
-        listener = block
-    }
-
 }
 
-
-typealias OnPasswordVisibilityClockListener = (Boolean, String) -> String
 
