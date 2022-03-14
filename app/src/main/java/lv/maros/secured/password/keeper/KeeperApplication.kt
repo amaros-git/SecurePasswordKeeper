53e8c5a44be9285d1a8917f537ce84276e17fe34
Anatolijs <a.morozovs@gmail.com>
2022-03-14 17:57:48 +0200

    some improvements for workmanager

--- app/src/main/java/lv/maros/secured/password/keeper/KeeperApplication.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/KeeperApplication.kt
@@ -1,13 +1,26 @@
 package lv.maros.secured.password.keeper
 
 import android.app.Application
+import androidx.work.Configuration
 import lv.maros.secured.password.keeper.data.PasswordDataSource
 import lv.maros.secured.password.keeper.data.local.PasswordsLocalRepository
 import lv.maros.secured.password.keeper.security.KeeperConfigStorage
 import lv.maros.secured.password.keeper.security.KeeperCryptor
 import timber.log.Timber
 
-class KeeperApplication : Application() {
+class KeeperApplication : Application(), Configuration.Provider {
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return if (BuildConfig.DEBUG) {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                .build()
+        } else {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.ERROR)
+                .build()
+        }
+    }
 
     val localPasswordsRepository: PasswordDataSource
         get() = ServiceLocator.provideLocalRepository(this)
@@ -21,6 +34,8 @@ class KeeperApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
