a1a127ffb3c988bcc6bb46f982bf6e05e067140b
Anatolijs <a.morozovs@gmail.com>
2022-03-24 13:24:11 +0200

    Working on generator dialog

--- app/src/main/java/lv/maros/secured/password/keeper/pages/addEdit/PasswordGeneratorDialog.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/pages/addEdit/PasswordGeneratorDialog.kt
@@ -1,13 +1,14 @@
 package lv.maros.secured.password.keeper.pages.addEdit
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import lv.maros.secured.password.keeper.R
 import lv.maros.secured.password.keeper.databinding.DialogPasswordGeneratorBinding
+import lv.maros.secured.password.keeper.security.KeeperPasswordManager
 import lv.maros.secured.password.keeper.utils.setDisplayHomeAsUpEnabled
 import lv.maros.secured.password.keeper.utils.setTitle
 import timber.log.Timber
@@ -19,19 +20,36 @@ class PasswordGeneratorDialog private constructor(
 
     private lateinit var binding: DialogPasswordGeneratorBinding
 
-    init {
-
-    }
-
     private fun configureViews() {
-        binding.passwordGeneratorCancelButton.setOnClickListener {
-            this.dismiss()
-        }
-
         binding.passwordGeneratorOkButton.setOnClickListener {
             viewModel.saveGeneratedPassword(getGeneratedPassword())
             this.dismiss()
         }
+
+        binding.passwordGeneratorCancelButton.setOnClickListener {
+            this.dismiss()
+        }
+
+        binding.passwordGeneratorGenerateButton.setOnClickListener {
+            generatePassword()
+        }
+
+        configureSpinner()
+    }
+
+    private fun configureSpinner() {
+        val intArray = intArrayOf(0, 1, 3).toTypedArray()
+
+        ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, intArray).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.passwordGeneratorPasswordLengthSpinner.adapter = it
+        }
+    }
+
+
+    private fun generatePassword() {
+        val password = KeeperPasswordManager.generatePassword(8)
+        Timber.d("password = $password")
     }
 
     private fun getGeneratedPassword(): String {
