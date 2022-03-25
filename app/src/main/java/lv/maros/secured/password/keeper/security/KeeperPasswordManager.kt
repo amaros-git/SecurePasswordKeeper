9c13d922ced3b7911fc15790f60631df85d1a81c
Anatolijs <a.morozovs@gmail.com>
2022-03-25 18:47:38 +0200

    in progress

--- app/src/main/java/lv/maros/secured/password/keeper/security/KeeperPasswordManager.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/security/KeeperPasswordManager.kt
@@ -1,10 +1,24 @@
 package lv.maros.secured.password.keeper.security
 
+import timber.log.Timber
+import kotlin.random.Random
+
 object KeeperPasswordManager {
+
+    private val allowedLetters = ('A'..'Z') + ('a'..'z')
+    private val allowedNumbers = ('0'..'9')
+    private val allowedSymbols = """
+        ~!@#$%^&*()_+"|<>?
+    """.trimIndent()
 
     private var useLetters: Boolean = true
     private var useDigits: Boolean = true
     private var useSymbols: Boolean = true
+
+    private fun getRandomSymbol(): Char {
+        val i = Random.nextInt(0, allowedSymbols.length)
+        return allowedSymbols[i]
+    }
 
     fun generateEncryptionKey(length: Int = 16): String {
         return "kF8g8hDDXvypd5KP"
@@ -15,10 +29,12 @@ object KeeperPasswordManager {
     }
 
     fun generatePassword(length: Int): String {
-        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        val char = getRandomSymbol()
+        /*val allowedChars = ('A'..'Z') +  +
 
         return (1..length)
             .map { allowedChars.random() }
-            .joinToString("")
+            .joinToString("")*/
+        return ""
     }
 }
