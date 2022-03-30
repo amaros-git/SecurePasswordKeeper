905e977a7a86888a3b9ed1c38183e09d864baad3
Anatolijs <a.morozovs@gmail.com>
2022-03-30 15:52:16 +0300

    in progress

--- app/src/main/java/lv/maros/secured/password/keeper/pages/passwords/PasswordsFragment.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/pages/passwords/PasswordsFragment.kt
@@ -47,7 +47,7 @@ class PasswordsFragment : BaseFragment() {
     private lateinit var passwordListAdapter: PasswordListAdapter
 
     private fun setupBottomNavigation() {
-        binding.bottomNavigation.uncheckAllItems()
+       /* binding.bottomNavigation.uncheckAllItems()
 
         binding.bottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
@@ -62,7 +62,7 @@ class PasswordsFragment : BaseFragment() {
                 }
                 else -> false
             }
-        }
+        }*/
     }
 
     private fun showSearchView() {
