45a3c51ac1c8cc74810bd65d28af85c11489a745
Anatolijs <a.morozovs@gmail.com>
2022-04-03 13:50:06 +0300

    in progress

---
+++ app/src/main/java/lv/maros/secured/password/keeper/SearchActivity.kt
@@ -0,0 +1,15 @@
+package lv.maros.secured.password.keeper
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+
+}
