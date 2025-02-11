7b9e947512036e6e53e3ddb8b1932c5ecc13b061
Anatolijs <a.morozovs@gmail.com>
2025-02-11 19:29:44 +0200

    Trying to rework the minimum of the code to be able to start project with compose and xml so I can continue further. Also took some new features from latest Android demo projects

--- app/src/main/java/lv/maros/secured/password/keeper/pages/setup/views/WelcomeFragment.kt
+++ app/src/main/java/lv/maros/secured/password/keeper/pages/setup/views/WelcomeFragment.kt
@@ -1,19 +1,25 @@
 package lv.maros.secured.password.keeper.pages.setup.views
 
 import android.os.Bundle
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import lv.maros.secured.password.keeper.R
 
 class WelcomeFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    override fun onCreateView(inflater: android.view.LayoutInflater, container: android.view.ViewGroup?, savedInstanceState: Bundle?): android.view.View {
+        return ComposeView(requireContext()).apply {
+            setContent { WelcomeScreen() }
+        }
     }
-
+}
+@Composable
+fun WelcomeScreen() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Text("Welcome")
+    }
 }
