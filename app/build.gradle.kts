94888094a6ba474cdecc202664deaaffeee7d9b2
Anatolijs <a.morozovs@gmail.com>
2025-03-03 21:51:36 +0200

    ono stroitsya,, but exception

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.nav.fragment.ktx)
     implementation(libs.timber)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.biometric)
+    implementation(libs.androidx.security.crypto)
 //    implementation(libs.androidx.room.ktx)
 //    implementation(libs.androidx.room.runtime)
 //    kapt(libs.androidx.room.compiler) // TODO move to KSP
